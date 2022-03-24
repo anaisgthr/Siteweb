d6f541256fce5402cbfd343119331777aa6cdac8
Anaïs GAUTHIER <agauthier@S031-029.local>
2022-03-24 11:39:17 +0100

    siteweb

---
+++ Crea-test.js
@@ -0,0 +1,23 @@
+const nom = 'Doe'
+const prenom = 'John'
+
+const nomEtPrenom = `Nom: ${nom} Prénom: ${prenom}`
+
+console.log(nom.length)
+console.log(nom.toUpperCase())
+console.log(nom.toLowerCase())
+console.log(nom.replace("D","D"))
+
+console.log(prenom[0])
+
+
+
+//"salut" === "Salut"
+
+const mot = 'salut'
+
+console.log(mot)
+console.log(mot.replace("s","S"))
+
+console.log(mot[0])
+
